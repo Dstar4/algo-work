607f6ceced775e8da68678c88f39d3fafce99bc1
Daniel <danielstarling4@gmail.com>
2019-08-01 20:04:49 -0700

    added getbyname

---
+++ scratch.js
@@ -0,0 +1,19 @@
+
+let obj = {
+    name: 'Krunal',
+    education: 'IT Engineer'
+};
+console.log(Object.keys(obj));
+
+let obj2 = {
+    name: 'Krunal',
+    education: 'IT Engineer'
+};
+console.log(Object.values(obj2));
+
+function anagrams(x, y) {
+    return (x === y ? true : false)
+}
+anagrams('racecar', 'racecar')
+
+
